267fe059167ea972c2617d941c803bec497d8684
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-03-13 08:37:19 -0300

    html

---
+++ numeros.js
@@ -0,0 +1,35 @@
+
+// Variáveis de controle
+
+// Obter os elementos
+
+// Atualizar a interface com o valor do slider
+
+//Exibir o valor do slider na interface
+
+// Eventos para atualizar o valor do slider ao ser alterado
+
+// Função para gerar um número aleatório
+
+// Atualizar a interface com o número sorteado
+
+// Criar um elemento "li"
+
+// Atribuir o número sorteado como conteúdo do "li"
+
+// Adicionar um evento de click ao "li"
+ 
+// Copiar o número sorteado para a área de transferência 
+
+// Exibir um alerta informando que o número foi copiado
+   
+// Adicionar o "li" no início da lista de números ("listaNumeros")
+  
+// Se o número de elementos na lista for maior que o limite, remover o último elemento
+  
+// Função para limpar o histórico de sorteios
+
+// Eventos
+
+
+
